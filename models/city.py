90f682c0d0ae2fa5a1ca5786d2e6db2f21cd65dd
sang-nguy0920 <2321@holbertonschool.com>
2021-02-16 20:37:16 +0000

    pep8  corrections

--- models/city.py
+++ models/city.py
@@ -5,6 +5,7 @@
 from models.base_model import BaseModel
 from models.engine import file_storage
 
+
 class City(BaseModel):
     """ class City that contains public attributes:
         state_id and name (both empty strings) """
