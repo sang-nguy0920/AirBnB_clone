5de1651cbca210ae2b9354cdad0d1e81bdda7b76
sang-nguy0920 <2321@holbertonschool.com>
2021-02-16 20:39:26 +0000

    pep8  corrections

--- models/user.py
+++ models/user.py
@@ -5,6 +5,7 @@
 from models.base_model import BaseModel
 from models.engine import file_storage
 
+
 class User(BaseModel):
     """ class User that contains public attributes: email, password,
         first_name and last_name (all are empty strings) """
